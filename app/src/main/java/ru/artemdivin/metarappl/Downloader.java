f0f488627d0d3f38a7a6cd7976e09264fea7ab9b
Gambini19 <divin@59.ru>
2017-05-03 18:01:32 +0500

    3 commit

--- app/src/main/java/ru/artemdivin/metarappl/Downloader.java
+++ app/src/main/java/ru/artemdivin/metarappl/Downloader.java
@@ -2,18 +2,21 @@ package ru.artemdivin.metarappl;
 
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Downloader extends AsyncTask<Void, Void, Void> {
 
     Map<String, String> data = new HashMap<>();
+    ArrayList<MetarObject> metarObjects = new ArrayList<>();
 
     @Override
     protected Void doInBackground(Void... params) {
@@ -24,14 +27,29 @@ public class Downloader extends AsyncTask<Void, Void, Void> {
             httpUrl = (HttpURLConnection) netURL.openConnection();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpUrl.getInputStream()));
             String s;
+            int count = 0;
             while ((s = bufferedReader.readLine()) != null) {
                 if (!s.isEmpty() && !(s.matches("^\\d{4}.+"))) {
                     String[] line = s.split(" ", 2);
-                    data.put(line[0], line[1]);
+                    Log.i("line0  ", line[0]);
+                    Log.i("line1  ", line[1]);
+                    //data.put(line[0], line[1]);
+                    metarObjects.add(MetarObject.getObj(line[0],line[1]));
+                    count++;
+                    Log.i("obj " , metarObjects.toString());
                 }
+                if (count == 20) break;
+
             }
-            System.out.println(data);
-        } catch (IOException e) {
+            httpUrl.disconnect();
+
+            Log.i ("list", String.valueOf(metarObjects));
+            //Log.i("metarObjects.size()  ", String.valueOf(metarObjects.size()));
+          //  Log.i("metarObjects.get(1)  ", String.valueOf(metarObjects.get(1)));
+
+
+
+        } catch (IOException  e) {
             e.printStackTrace();
         }
 
--- app/src/main/java/ru/artemdivin/metarappl/MetarObject.java
+++ app/src/main/java/ru/artemdivin/metarappl/MetarObject.java
@@ -1,23 +1,21 @@
 package ru.artemdivin.metarappl;
 
-import java.util.Map;
-
 /**
  * Created by Kondratiy on 30.04.2017.
  */
 
 public class MetarObject {
     private String nameAirport;
-    private int termo;
-    private int pressure;
-    private String wind;
-    private String visibility;
-    private String cloud;
+    private static String termo;
+    private static String pressure;
+    private static String wind;
+    private static String visibility;
+    private static String cloud;
 
-    private String termoString = null;
-    private String pressureString = null;
+    private static String termoString = null;
+    private static String pressureString = null;
 
-    public MetarObject(String nameAirport, int termo, int pressure, String wind, String visibility, String cloud) {
+    public MetarObject(String nameAirport, String termo, String pressure, String wind, String visibility, String cloud) {
         this.nameAirport = nameAirport;
         this.termo = termo;
         this.pressure = pressure;
@@ -26,29 +24,30 @@ public class MetarObject {
         this.cloud = cloud;
     }
 
-    public String GetCode(Map map) {
+    /*public String GetCode(Map map) {
         nameAirport = String.valueOf(map.get("EditText Field"));
         return nameAirport;
-    }
+    }*/
 
-    public String  getMeteo(String line) {
+    public static String  getMeteo(String line) {
+        line = "KMYJ 021355Z AUTO 27013G17KT 10SM CLR 10/05 A2988 RMK AO2 ";
         String[] masData = line.split(" ");
 
 
         for (String s : masData
                 ) {
             //Температура
-            if (s.matches("\\d\\d\\\\\\d\\d")) return termoString = (s.substring(0, 3));
+            if (s.matches("\\d\\d\\\\\\d\\d")) return termo = (s.substring(0, 3));
             //else if (s.matches("M\\d\\d\\\\M?\\d\\d"))  return  termoString = (s.substring(1,4))*(-1);
-            else if (s.matches("M\\d\\d\\\\M?\\d\\d"))  return  termoString = (s.substring(1,4));
+            else if (s.matches("M\\d\\d\\\\M?\\d\\d"))  return  termo = (s.substring(1,4));
             //else termo = Integer.MAX_VALUE;
 
             //Давление
             /*if (s.matches("Q\\d{4}")) pressure = Integer.parseInt(s.substring(1,6));
             else if (s.matches("A\\d{4}")) pressure= (int) (Integer.parseInt(s.substring(1,6))*(0.3386));*/
 
-            if (s.matches("Q\\d{4}")) return  pressureString =(s.substring(1,6));
-            else if (s.matches("A\\d{4}")) return  pressureString= (s.substring(1,6));
+            if (s.matches("Q\\d{4}")) return  pressure =(s.substring(1,5));
+            else if (s.matches("A\\d{4}")) return  pressure= (s.substring(1,5));
 
             //Ветер
             if (s.matches("\\d{5}MPS"))
@@ -58,12 +57,12 @@ public class MetarObject {
 
             //Видимость
             if ( s.matches("\\d{4}")) return visibility = s;
-            else if (s.matches("\\d{2}SM"))return visibility = s.substring(0,3) + " Statute Miles" ;
+            else if (s.matches("\\d{1,2}SM"))return visibility = s.substring(0,3) + " Statute Miles" ;
           //  else return visibility = "нет информации";
 
             //Облачность
 
-            if (s.matches("CAVOK") || s.equals("NSC") || s.equals("NCD"))return cloud = "Sky is clear";
+            if (s.matches("CAVOK") || s.equals("NSC") || s.equals("NCD") || s.equals("CLR"))return cloud = "Sky is clear";
             else if  (s.matches("SCT\\d{3}") || s.matches("FEW\\d{3}"))return cloud = "Малооблачно";
             else if (s.matches("BKN\\d{3}") || s.matches("OVC\\d{3}"))return cloud = "Облачно";
             //else return cloud = "нет информации";
@@ -72,19 +71,90 @@ public class MetarObject {
         return line;
     }
 
-    public  MetarObject getObj (Map <String, String> map){
-        nameAirport = String.valueOf(map.get("EditText Field"));
-        getMeteo(map.get(nameAirport));
+    public static void setTermo(String termo) {
+        MetarObject.termo = termo;
+    }
+
+    public static void setPressure(String pressure) {
+        MetarObject.pressure = pressure;
+    }
+
+    public static void setWind(String wind) {
+        MetarObject.wind = wind;
+    }
+
+    public static void setVisibility(String visibility) {
+        MetarObject.visibility = visibility;
+    }
+
+    public static void setCloud(String cloud) {
+        MetarObject.cloud = cloud;
+    }
+
+    public static void setTermoString(String termoString) {
+        MetarObject.termoString = termoString;
+    }
+
+    public static void setPressureString(String pressureString) {
+        MetarObject.pressureString = pressureString;
+    }
+
+    public static String getTermo() {
+        return termo;
+    }
+
+    public static String getPressure() {
+        return pressure;
+    }
+
+    public static String getWind() {
+        return wind;
+    }
+
+    public static String getVisibility() {
+        return visibility;
+    }
+
+    public static String getCloud() {
+        return cloud;
+    }
+
+    public static String getTermoString() {
+        return termoString;
+    }
+
+    public static String getPressureString() {
+        return pressureString;
+    }
+
+    public static   MetarObject getObj (String codeAirport, String w){
+        /*nameAirport = String.valueOf(map.get("EditText Field"));
+        getMeteo(map.get(nameAirport));*/
+        setCloud(getMeteo(w));
+        setTermo((getMeteo(w)));
+        setPressure((getMeteo(w)));
+        setVisibility(getMeteo(w));
+        setWind(getMeteo(w));
+
 
         return new MetarObject
-                        (nameAirport
-                        ,Integer.parseInt(termoString)
-                        ,Integer.parseInt(pressureString)
-                        ,getMeteo(map.get(nameAirport))
-                        ,getMeteo(map.get(nameAirport))
-                        ,getMeteo(map.get(nameAirport)));
+                        (codeAirport
+                        ,getTermo()
+                        ,getPressure()
+                        ,getWind()
+                        ,getVisibility()
+                        ,getCloud());
+    }
 
-
-        
+    @Override
+    public String toString() {
+        return "MetarObject{" +
+                " nameAirport=  '" + nameAirport + '\'' +
+                "  температура " + getTermo() + "\n" +
+                "  Давление  " + getPressure() + "\n" +
+                "  Ветер  " + getWind() + "\n" +
+                "  Видимость " + getVisibility() + "\n" +
+                "  Облачность " + getCloud()+ "\n" +
+                '}';
     }
 }
