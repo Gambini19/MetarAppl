3522a5f8ff2087c58810016e2c9614b7be71eede
Gambini19 <divin@59.ru>
2017-05-04 17:48:57 +0500

    9

--- app/src/main/java/ru/artemdivin/metarappl/MainActivity.java
+++ app/src/main/java/ru/artemdivin/metarappl/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnShowWether.setOnClickListener(this);
         adapter = new MetarAdapter(this);
-
+///TESTSTSTSTT
     }
 
     @Override
